ffe110093a691bf8423b35da992199cfdb3e9110
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-08-26 10:28:56 +0530

    FindAllDuplicateInAnArray.cpp

---
+++ FindAllDuplicateInAnArray.cpp
@@ -0,0 +1,12 @@
+class solution{
+public:
+vector<int>findDuplicates(vector<int>nums){
+    vector<int>ans;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size;i++){
+      if(nums[i]==nums[i+1){
+        ans.push_back(nums[i]);
+      }
+    }
+  return ans;
+}
