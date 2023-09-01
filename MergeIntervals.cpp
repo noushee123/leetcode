112a00cb0f8c22f4b2c9a045749c61d12425ec50
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-09-01 23:27:32 +0530

    MergeIntervals.cpp

---
+++ MergeIntervals.cpp
@@ -0,0 +1,29 @@
+class solution{
+public:
+
+vector<vector<int>> merge(vector<vector<int>>& intervals){
+
+  int n = intervals.size();
+  sort(intervals.begin(),intervals.end());
+
+  vector<vector<int>> ans;
+  for(int i=0;i<n;i++){
+    if(ans.empty()){
+      ans.push_back(intervals[i]);
+    }
+    else{
+        vector<int>& v = ans.back();
+        int y = v[1];
+
+        if(y>=intervals[i][0]){
+            v[1] = max(y,intervals[i][1]);
+        }
+      else{
+          ans.push_back(intervals[i]);
+      }
+    }
+  }
+  return ans;
+}
+
+};
