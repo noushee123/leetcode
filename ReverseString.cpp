c54c9342d21f4fd6c2fe2924bd681fa96795ebf7
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-09-12 22:49:48 +0530

    ReverseString.cpp

---
+++ ReverseString.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int i=0,j=n-1;
+        while(i<j){
+            
+            int temp = s[i];
+            s[i] = s[j];
+            s[j]=temp;
+            i++,j--;
+        }
+        for(int i=0;i<n;i++)
+        cout<<s[i]<<",";
+    }
+};
