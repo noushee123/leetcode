ef6b424b1088410910e35eebc997dee3237a290f
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-09-20 23:19:05 +0530

    RotateString.cpp

---
+++ RotateString.cpp
@@ -0,0 +1,15 @@
+class solution{
+public:
+     bool rotateString(string s,string goal){
+       int k=s.length();
+       while(k--){
+         char ch = s[0];
+         s.erase(s.begin());
+         s.push_back(ch);
+
+         if(s==goal)
+           return true;
+       }
+       return false;
+     }
+};
