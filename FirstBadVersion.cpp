5042a204ecae09324c68a5ca0b14dcecd4fc8113
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-09-28 20:27:43 +0530

    FirstBadVersion.cpp

---
+++ FirstBadVersion.cpp
@@ -0,0 +1,16 @@
+class solution{
+public:
+int firstBadVersion(int n){
+  int s = 1;
+  int e = n;
+  while(s<e){
+    int mid = s+(e-s)/2;
+    if(isBadVersion(mid)){
+       e=mid;
+    }
+    else{
+      s=mid+1;
+    }
+    return s;
+  }
+};
