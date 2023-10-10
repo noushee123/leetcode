8b5205c2411f04bcbf9a07c87b4ddb22e2fb4d68
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-10-11 00:03:33 +0530

    ThirdMaximumNumber.cpp

---
+++ ThirdMaximumNumber.cpp
@@ -0,0 +1,26 @@
+class solution{
+public:
+    int thirdMax(vector<int>& nums){
+      sort(nums.begin(),nums.end());
+      int n=nums.size();
+      int count = 0;
+      int third_maximum = 0;
+      for(int i=n-1;i>0;i--){
+          if(nums[i]!=nums[i-1]){
+               count++;
+               third_maximum=nums[i];
+          }
+          else if(i==1 && nums[i]==nums[i-1]){
+                count++;
+                third_maximum=nums[i];
+          }
+          if(count>2){
+                return third_maximum;
+            }
+      }
+       if(count+1==3 && nums[0]!=nums[1]){
+              return nums[0];
+         }
+      return nums[n-1];
+    }
+}
