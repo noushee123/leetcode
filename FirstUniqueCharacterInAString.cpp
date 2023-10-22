dcd1c6de418108575893fe2dcfbe7eecfd6f9008
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-10-22 23:48:38 +0530

    FirstUniqueCharacterInAString.cpp

---
+++ FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+      int n=s.size();
+      for(int i=0;i<n;i++){
+        bool flag = 0;
+      for(int j=0;j<n;j++){
+        if(s[i]==s[j] && i!=j){
+          flag = 1;
+          break;
+        }
+      }
+       if(flag == 0){
+          return i;
+       }
+      }
+      return -1;
+    }
+}
