fb08e0162bbc0aba2724eda626de587fc64409dd
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-11-03 00:37:04 +0530

    FindMaximumNumberOfString.cpp

---
+++ FindMaximumNumberOfString.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        string s;
+        int count = 0;
+        for(int i=0;i<words.size()-1;i++)
+        {
+            for(int j=i+1;j<words.size();j++)
+            {
+                s = words[j];
+                reverse(s.begin(),s.end());
+                if(words[i]==s)    count++;
+            }
+        }
+        return count;
+    }
+};
